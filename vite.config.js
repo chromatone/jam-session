94da1bdf514a7e5750a7989512229a1df002bb47
davay <davay@defucc.me>
2025-01-18 22:49:18 +0700

    feat: better vite

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from "url";
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import { transformerDirectives, presetIcons, presetUno, extractorSplit } from 'unocss'
 import extractorPug from '@unocss/extractor-pug'
-
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
   base: './',
@@ -18,6 +17,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    viteSingleFile(),
     Unocss({
       transformers: [
         transformerDirectives(),
@@ -53,47 +53,36 @@ export default defineConfig({
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       exclude: [/node_modules/, /\.git/],
     }),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-    //   manifest: {
-    //     name: 'TouchMe experience',
-    //     short_name: 'Journey to the music of touch',
-    //     description: 'Playtronica and Chromatone collaboration',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: 'apple-touch-icon.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'apple-touch-icon.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'apple-touch-icon.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable',
-    //       }
-    //     ]
-    //   }
-    // }),
+    viteBuildScript(),
   ],
   optimizeDeps: {
     include: ['vue', '@vueuse/core'],
   },
-
-  build: {
-    cssCodeSplit: false,
-    sourcemap: true,
-    chunkSizeWarningLimit: Infinity,
-    rollupOptions: {
-      manualChunks: {
-        'use': ['@vueuse/core', '@vueuse/math', '@vueuse/gesture']
-      }
-    }
-  },
 })
+
+
+function viteBuildScript() {
+  return {
+    name: 'vite-build-script',
+    transformIndexHtml(html) {
+      if (process.env.NODE_ENV === 'production') {
+        return html.replace(/<!-- Stats production build insert -->/, `<script async defer src="https://stats.chromatone.center/script.js" data-website-id="53f3de31-a17e-407b-a81b-a151737f0cb7"></script>
+          
+          <script>
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+          .then((registration) => {
+            console.log('Service Worker registered with scope: ', registration.scope);
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed: ', error);
+          });
+      });
+    }
+  </script>`);
+      }
+      return html;
+    },
+  };
+}
